c2dd5c72327ac894c541a2fed27ef045c4c8a9be
Shireen Gadru <shiru411@gmail.com>
2022-06-09 20:01:22 +0530

    added recorder component

--- src/components/Recorder/Recorder.tsx
+++ src/components/Recorder/Recorder.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import './Recorder.css';
-import { selectDateStart, start } from '../../redux/recorder';
+import { selectDateStart, start, stop } from '../../redux/recorder';
 import { useSelector } from 'react-redux';
 import cx from 'classnames';
+
+const addZero = (num: number) => (num < 10 ? `0${num}` : `${num}`);
 
 const Recorder = () => {
   const dispatch = useDispatch();
   const dateStart = useSelector(selectDateStart);
   const started = dateStart !== '';
-
+  let interval = useRef<number>(0);
+  const [, setCount] = useState<number>(0);
   const handleClick = () => {
-    dispatch(start());
+    if (started) {
+      window.clearInterval(interval.current);
+      dispatch(stop());
+    } else {
+      dispatch(start());
+      interval.current = window.setInterval(() => {
+        setCount((count) => count + 1);
+      }, 1000);
+    }
   };
 
+  useEffect(() => {
+    return () => {
+      window.clearInterval(interval.current);
+    };
+  }, []);
+
+  let seconds = started
+    ? Math.floor((Date.now() - new Date(dateStart).getTime()) / 1000)
+    : 0;
+  const hours = seconds ? Math.floor(seconds / 60 / 60) : 0;
+  seconds -= hours * 60 * 60;
+  const minutes = seconds ? Math.floor(seconds / 60) : 0;
+  seconds -= minutes * 60;
   return (
-    <div className={cx('recorder', {'recorder-started': started})}>
+    <div className={cx('recorder', { 'recorder-started': started })}>
       <button className="recorder-record" onClick={handleClick}>
         <span></span>
       </button>
 
-      <div className="recorder-counter">00:00:00</div>
+      <div className="recorder-counter">
+        {addZero(hours)}:{addZero(minutes)}:{addZero(seconds)}
+      </div>
     </div>
   );
 };
